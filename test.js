f954d573a4bc8d20ae6727632d97d2b242b61e08
Richard Guerre <richardmguerre@gmail.com>
2023-05-20 18:26:25 +0800

    Remove unnecessary files

--- test.js
+++
@@ -1,2 +0,0 @@
-export const test = 'hello world from external file';
-export const testFn = () => 'hello world test fn';
--- test2.js
+++
@@ -1,1 +0,0 @@
-export const testFn = () => console.log("some global value", someGlobalValue);
