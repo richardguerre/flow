077876d05afea99da42f153f17fa79ef059b5214
Richard Guerre <richardmguerre@gmail.com>
2023-05-20 09:51:40 +0800

    Add routine steps for shutdown

--- packages/flow-essentials/src/web.tsx
+++ packages/flow-essentials/src/web.tsx
@@ -33,6 +33,7 @@ export default definePlugin((options) => {
   return {
     slug: "flow-essentials",
     routineSteps: {
+      // Morning routine steps
       "intro-to-yesterday": {
         component: (props) => <TextTransitionStep {...props}>Yesterday</TextTransitionStep>,
       },
@@ -204,6 +205,123 @@ export default definePlugin((options) => {
               />
               <props.BackButton />
               <props.NextButton />
+            </div>
+          );
+        },
+      },
+      // Shutdown routine steps
+      "intro-to-todays-shutdown": {
+        component: (props) => (
+          <TextTransitionStep {...props}>Let's reflect on what you did today</TextTransitionStep>
+        ),
+      },
+      "clean-up-today": {
+        component: (props) => {
+          const today = options.dayjs();
+          const [days, loadingDays] = options.hooks.useAsyncLoader(async () => {
+            return await options.getDays({
+              from: today.toDate(),
+              to: today.toDate(),
+              toRender: { Day: true },
+            });
+          });
+
+          const day = days?.[0];
+
+          if (loadingDays) {
+            return <>Loading...</>;
+          }
+
+          return (
+            <div>
+              <div>
+                <props.BackButton />
+                <props.NextButton />
+              </div>
+              <Flow.Day day={day} label="Today" />
+            </div>
+          );
+        },
+      },
+      "retro-on-today": {
+        component: (props) => {
+          const [initialValue, setInitialValue] = useState<string | null>(null);
+          const today = options.dayjs();
+
+          useEffect(() => {
+            (async () => {
+              const days = await options.getDays({
+                from: today.toDate(),
+                to: today.toDate(),
+                include: { tasks: true },
+              });
+              if (!days.length) {
+                setInitialValue("");
+                return;
+              }
+              const day = days[0];
+              /**
+               * Example output:
+               * <ul>
+               *  <li>✅ Task 1</li>
+               *  <li>❌ Task 2</li>
+               *  <li>Task 3</li>
+               * </ul>
+               */
+              setInitialValue(
+                `<ul>${day.tasks
+                  .map(
+                    (task) =>
+                      `<li>${
+                        task.status === "DONE" ? "✅ " : task.status === "CANCELED" ? "❌ " : ""
+                      }${task.title}</li>`
+                  )
+                  .join("")}</ul>`
+              );
+            })();
+          }, []);
+
+          return (
+            <div>
+              <Flow.NoteEditor
+                slug={`flow-essentials_retro-${today.format("YYYY-MM-DD")}`}
+                title={`Retro of ${today.format("MMMM D")}`}
+                loading={initialValue === null}
+                initialValue={initialValue ?? ""}
+              />
+              <props.BackButton />
+              <props.NextButton />
+            </div>
+          );
+        },
+      },
+      "intro-to-tomorrow": {
+        component: (props) => <TextTransitionStep {...props}>Tomorrow</TextTransitionStep>,
+      },
+      "plan-for-tomorrow": {
+        component: (props) => {
+          const tomorrow = options.dayjs().add(1, "day");
+          const [days, loadingDays] = options.hooks.useAsyncLoader(async () => {
+            return await options.getDays({
+              from: tomorrow.toDate(),
+              to: tomorrow.toDate(),
+              toRender: { Day: true },
+            });
+          });
+
+          const day = days?.[0];
+
+          if (loadingDays) {
+            return <>Loading...</>;
+          }
+
+          return (
+            <div>
+              <div>
+                <props.BackButton />
+                <props.NextButton />
+              </div>
+              <Flow.Day day={day} label="Tomorrow" />
             </div>
           );
         },
