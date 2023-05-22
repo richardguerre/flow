c7bbeeb79920d9d50e1a86c483e7730fc16248d7
Richard Guerre <richardmguerre@gmail.com>
2023-05-22 11:04:48 +0800

    Clean up server start

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,6 @@
 import express from "express";
 import { createYoga } from "graphql-yoga";
 import { schema } from "./graphql";
-import { externalSources } from "../../../config";
-import { addItems } from "./utils/addItems";
-import { pluralize } from "./utils/pluralize";
 import { getPlugins } from "./utils/getPlugins";
 
 const PORT = process.env.PORT ?? 4000;
@@ -35,46 +32,12 @@ app.use("/api/:pluginSlug", async (req, res, next) => {
   next();
 });
 
-app.post("/webhook/:name", async (req, res) => {
-  console.log("Received webhook", req.params.name, req.body);
-  try {
-    if (!req.params.name) throw "No name provided for Webhook";
-    const externalSourceName = Object.keys(externalSources).find((name) => {
-      const externalSource = externalSources[name];
-      const webhookName = externalSource?.webhook?.name ?? name;
-      return webhookName.toLowerCase() === req.params.name.toLowerCase();
-    });
-    if (!externalSourceName) {
-      throw "No external source found with name " + req.params.name + "in config.";
-    }
-    const externalSource = externalSources[externalSourceName];
-    let items = (await externalSource?.webhook?.onWebhookEvent(req.body)) ?? null;
-    if (!items) throw "Webhook event is not relevant";
-    if (!Array.isArray(items)) items = [items];
-
-    const result = await addItems(externalSourceName, items);
-    console.log(
-      `Added ${result.count} external ${pluralize("item", result.count)} from ${externalSourceName}`
-    );
-  } catch (e: any) {
-    console.log("Webhook error:", e);
-  }
-  res.sendStatus(200);
-});
-
 // -------------------------- Server ------------------------------
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
     console.log(`\n✅ Server started on port ${PORT}`);
-    console.log(`GraphQL API: http://localhost:${PORT}/graphql`);
-    for (const name in externalSources) {
-      console.log(
-        `Webhook: http://localhost:${PORT}/webhook/${(
-          externalSources[name]?.webhook?.name ?? name
-        ).toLowerCase()}`
-      );
-    }
+    console.log(`🟣 GraphQL API: http://localhost:${PORT}/graphql`);
   });
 } else {
   // express will default to port 0 which will randomly assign a port
