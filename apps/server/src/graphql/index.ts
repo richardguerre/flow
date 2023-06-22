55266bab60ce5aea76b897619fb39a51c87ddd6c
Richard Guerre <richardmguerre@gmail.com>
2023-06-22 14:32:49 +0800

    Generate prisma client before generating GraphQL schema

--- apps/server/src/graphql/index.ts
+++ apps/server/src/graphql/index.ts
@@ -20,4 +20,5 @@ if (process.env.NODE_ENV === "development") {
   const schemaAsString = printSchema(schema);
   const path = join(__dirname, "../../../web/src/relay/schema.graphql");
   writeFileSync(path, schemaAsString);
+  console.log("✅ GraphQL schema generated into apps/web/src/relay/schema.graphql");
 }
