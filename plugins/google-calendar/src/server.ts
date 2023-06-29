6dcdba32d1cad26331f3c9c9fa505eccf434333d
Richard Guerre <richardmguerre@gmail.com>
2023-06-29 23:22:06 +0800

    Add expires_at in tokens secret for google-calendar-plugin

--- plugins/google-calendar/src/server.ts
+++ plugins/google-calendar/src/server.ts
@@ -8,7 +8,14 @@ export default definePlugin("google-calendar", (opts) => {
         return res.redirect("https://google-calendar.vercel.com/api/auth"); // TODO: replace with the actual URL of the plugin api
       } else if (req.path === "/auth/callback" && req.method === "POST") {
         // store the access token in the user's Flow instance and return 200
-        const tokenData = req.body;
+        const tokenData = {
+          ...req.body,
+          expires_at: opts
+            .dayjs()
+            .add((req.body.expires_in ?? 10) - 10, "seconds") // 10 seconds buffer to account for latency in network requests
+            .toISOString(),
+        };
+        delete tokenData.expires_in;
         await opts.store.setSecretItem(TOKENS_STORE_KEY, tokenData);
         return res.status(200).send();
       }
