176723d944b9fcea237387b58d4fab9398b6c5d2
Richard Guerre <richardmguerre@gmail.com>
2023-08-19 17:12:38 +0800

    Add list of plugins

--- apps/web/src/views/BrowsePluginsView.tsx
+++ apps/web/src/views/BrowsePluginsView.tsx
@@ -10,6 +10,40 @@ import { BrowsePluginsViewInstallFromUrlMutation } from "@flowdev/web/relay/__ge
 import { useState } from "react";
 import { toast } from "@flowdev/ui/Toast";
 
+type PluginAuthor = {
+  name: string;
+  avatarUrl?: string;
+};
+
+type Plugin = {
+  iconUrl: string;
+  name: string;
+  description: string;
+  installUrl: string;
+  version: string;
+  authors: PluginAuthor[]; // first author is the primary author
+};
+
+const PLUGINS: Plugin[] = [
+  {
+    iconUrl: "FlowIcon.svg",
+    name: "Essentials",
+    description:
+      "The official and default plugin for Flow containing essential features such as a morning routine and a shutdown routine.",
+    installUrl: "https://cdn.jsdelivr.net/npm/@flowdev/essentials@0.1.0/out",
+    version: "0.1.0",
+    authors: [{ name: "Flow", avatarUrl: "FlowIcon.svg" }],
+  },
+  {
+    iconUrl: "https://ssl.gstatic.com/calendar/images/dynamiclogo_2020q4/calendar_19_2x.png",
+    name: "Google Calendar",
+    description: "Official Google Calendar plugin for Flow.",
+    installUrl: "https://cdn.jsdelivr.net/npm/@flowdev/google-calendar@0.1.0/out",
+    version: "0.1.0",
+    authors: [{ name: "Flow", avatarUrl: "FlowIcon.svg" }],
+  },
+];
+
 export default () => {
   const [openInstallPluginFromUrl, setOpenInstallPluginFromUrl] = useState(false);
 
@@ -19,7 +53,7 @@ export default () => {
         <div className="w-full text-center text-3xl font-extrabold">Browse plugins</div>
         <div className="w-full text-center text-base">Sprinkle a little magic in your day</div>
       </div>
-      <div className="gap-4">
+      <div className="flex flex-col gap-4">
         <div className="bg-background-50 flex items-center justify-between gap-3 rounded-md p-3">
           <Tooltip delayDuration={100}>
             <TooltipTrigger>
@@ -42,6 +76,38 @@ export default () => {
               <InstallPluginFromUrlForm onClose={() => setOpenInstallPluginFromUrl(false)} />
             </PopoverContent>
           </Popover>
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+          {PLUGINS.map((plugin) => (
+            <div
+              key={plugin.name}
+              className="bg-background-50 min-w-xs flex flex-col gap-2 rounded p-4 shadow-md"
+            >
+              <div className="flex flex-col gap-1">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-2">
+                    <img src={plugin.iconUrl} className="h-5 w-5" />
+                    <div className="text-base font-medium">{plugin.name}</div>
+                  </div>
+                  <Button secondary>Install</Button>
+                </div>
+                <div className="text-foreground-700 text-sm">{plugin.description}</div>
+              </div>
+              <div className="text-foreground-700 flex items-center gap-4 text-sm">
+                <div className="flex gap-2">v{plugin.version}</div>
+              </div>
+              <div className="text-foreground-900 flex items-center gap-2 text-sm">
+                {plugin.authors[0].avatarUrl && (
+                  <img
+                    src={plugin.authors[0].avatarUrl}
+                    className="ring-primary-100 inline-block h-5 w-5 rounded-full ring"
+                  />
+                )}
+                {plugin.authors[0].name}
+                {plugin.authors.length > 1 && ` & ${plugin.authors.length - 1} more`}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
