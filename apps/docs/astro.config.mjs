ab6416911b8a3bfa716ee3f433972b1094591323
Richard Guerre <richardmguerre@gmail.com>
2024-03-29 02:02:23 +0200

    Add tailwind + landing page updates

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import UnoCSS from "unocss/astro";
+import unoConfig from "@flowdev/unocss";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    UnoCSS({
+      configResolved: () => unoConfig,
+    }),
     starlight({
       title: "Flow",
       social: {
