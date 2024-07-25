6e1a9e9966351048b26c0c70b6e7d91b6fdd1eae
Richard Guerre <richardmguerre@gmail.com>
2024-07-25 14:07:25 +0200

    vite pwa CLI output

---
+++ apps/mobile-pwa/vite.config.ts
@@ -0,0 +1,39 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "Flow Mobile PWA",
+        short_name: "Flow PWA",
+        description: "PWA resolved when using a mobile to login to a user's Flow app.",
+        theme_color: "#4F46E5",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
